91d83942bd50422d2c7393a7d416f2bd46215fb7
John Feltz <john.feltz@kumanu.com>
2023-06-21 09:01:39 -0400

    fixes scope

--- app/models/event.rb
+++ app/models/event.rb
@@ -24,7 +24,7 @@ class Event < ApplicationRecord
 
     # scopes
     scope :ascending, -> { order(date: :asc) }
-    scope :descending, -> { order(date: desc) }
+    scope :descending, -> { order(date: :desc) }
 
     # class methods
 
