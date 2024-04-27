f7422f416e4cd0c66ee356a116a76a8f11becf2d
John Feltz <john.feltz@kumanu.com>
2024-04-27 11:23:20 -0400

    fixed inheritance in migration file

--- db/migrate/20240427110600_add_season_to_crops.rb
+++ db/migrate/20240427110600_add_season_to_crops.rb
@@ -1,6 +1,6 @@
 # Full history of water level readings for all tanks
 #
-class CreateLevelReadings < ActiveRecord::Migration[7.0]
+class AddSeasonToCrops < ActiveRecord::Migration[7.0]
   def change
     add_column :crops, :season, :integer, :null => false, :default => 0
   end
