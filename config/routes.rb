53ed3a85cbd6e1cb9e742e5dfcb81533ef16f145
John Feltz <johncfeltz@gmail.com>
2025-04-23 17:52:02 -0400

    Converts split action to GET; Rails 7 doesnt allow easy override of link_to ... method: :post anymore

--- config/routes.rb
+++ config/routes.rb
@@ -15,7 +15,7 @@ Rails.application.routes.draw do
   resources :moisture_readings
   resources :crops do
     get 'past'
-    post 'split'
+    get 'split'
   end
   resources :events
   resources :notes
